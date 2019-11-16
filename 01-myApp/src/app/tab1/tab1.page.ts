b195e26fad9d61a1da033c61aba0a6b8f1d3aa5a
vanfigo <rodrigo.loyola@live.com>
2019-11-15 22:27:21 -0600

    debugger line and ionic lab

--- 01-myApp/src/app/tab1/tab1.page.ts
+++ 01-myApp/src/app/tab1/tab1.page.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
 
   constructor() {}
+
+  ngOnInit() {
+    let number = 10;
+    // tslint:disable-next-line: no-debugger
+    debugger;
+    number += 10;
+    number *= 2;
+    number -= 10;
+    console.log({number});
+  }
 
 }
