888c59f6335881068931743d30aadf129f253db3
vanfigo <rodrigo.loyola@live.com>
2019-11-16 12:44:34 -0600

    ion headers

--- 03-components/src/app/app-routing.module.ts
+++ 03-components/src/app/app-routing.module.ts
@@ -2,16 +2,10 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule) },
-  {
-    path: 'alert',
-    loadChildren: () => import('./pages/alert/alert.module').then( m => m.AlertPageModule)
-  },
-  {
-    path: 'action-sheet',
-    loadChildren: () => import('./pages/action-sheet/action-sheet.module').then( m => m.ActionSheetPageModule)
-  }
+  { path: 'alert', loadChildren: () => import('./pages/alert/alert.module').then( m => m.AlertPageModule) },
+  { path: 'action-sheet', loadChildren: () => import('./pages/action-sheet/action-sheet.module').then( m => m.ActionSheetPageModule) },
+  { path: '', redirectTo: 'home', pathMatch: 'full' }
 ];
 
 @NgModule({
