2219c2d6c16f273950fa3600d69d464744d8dac9
vanfigo <rodrigo.loyola@live.com>
2019-11-17 18:04:40 -0600

    buttons

--- 03-components/src/app/app-routing.module.ts
+++ 03-components/src/app/app-routing.module.ts
@@ -6,7 +6,11 @@ const routes: Routes = [
   { path: 'home', loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule) },
   { path: 'alert', loadChildren: () => import('./pages/alert/alert.module').then( m => m.AlertPageModule) },
   { path: 'action-sheet', loadChildren: () => import('./pages/action-sheet/action-sheet.module').then( m => m.ActionSheetPageModule) },
-  { path: 'avatar', loadChildren: () => import('./pages/avatar/avatar.module').then( m => m.AvatarPageModule) }
+  { path: 'avatar', loadChildren: () => import('./pages/avatar/avatar.module').then( m => m.AvatarPageModule) },
+  {
+    path: 'buttons',
+    loadChildren: () => import('./pages/buttons/buttons.module').then( m => m.ButtonsPageModule)
+  }
 ];
 
 @NgModule({
--- 03-components/src/app/pages/home/home.page.ts
+++ 03-components/src/app/pages/home/home.page.ts
@@ -10,7 +10,8 @@ export class HomePage implements OnInit {
   components: AngularComponent[] = [
     { icon: 'appstore', name: 'Alert', redirectTo: '/alert' },
     { icon: 'american-football', name: 'Action Sheet', redirectTo: '/action-sheet' },
-    { icon: 'beaker', name: 'Avatar', redirectTo: '/avatar' }
+    { icon: 'beaker', name: 'Avatar', redirectTo: '/avatar' },
+    { icon: 'radio-button-on', name: 'Button and Routes', redirectTo: '/buttons' }
   ];
 
   constructor() { }
