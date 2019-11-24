2d65ab0423b241c4784f39e4dc28710080eb3336
vanfigo <rodrigo.loyola@live.com>
2019-11-23 18:03:59 -0600

    toast

--- 03-components/src/app/pages/list/list.page.ts
+++ 03-components/src/app/pages/list/list.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { DataService } from 'src/app/providers/data.service';
 import { Observable } from 'rxjs';
-import { IonList } from '@ionic/angular';
+import { IonList, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-list',
@@ -13,21 +13,30 @@ export class ListPage implements OnInit {
   @ViewChild(IonList, { static: false }) list: IonList;
   users: Observable<any>;
 
-  constructor(private dataService: DataService) { }
+  constructor(private dataService: DataService,
+              private toastController: ToastController) { }
 
   ngOnInit() {
     this.users = this.dataService.getUsers();
   }
 
+  presentToast = async (message: string) => {
+    const toast = await this.toastController.create({ message, duration: 2000 });
+    await toast.present();
+  }
+
   favorite = () => {
-    console.log('favorite');
+    this.presentToast('You have clicke Favorite');
     this.list.closeSlidingItems();
   }
 
   share = () => {
-    console.log('share');
+    this.presentToast('You have clicke Share');
     this.list.closeSlidingItems();
   }
-  unread = () => console.log('unread');
+  unread = () => {
+    this.presentToast('You have clicke Delete');
+    this.list.closeSlidingItems();
+  }
 
 }
