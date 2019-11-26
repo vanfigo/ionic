e0d4fbb1e4341c857e51ec2c934fd0a1541b7128
vanfigo <rodrigo.loyola@live.com>
2019-11-25 21:46:59 -0600

    android build and tabs fix

--- 03-components/src/app/pages/tabs/tabs-routing.module.ts
+++ 03-components/src/app/pages/tabs/tabs-routing.module.ts
@@ -6,9 +6,12 @@ import { TabsPage } from './tabs.page';
 const routes: Routes = [
   { path: '', redirectTo: 'contact' },
   { path: '', component: TabsPage, children: [
-    { path: 'account', loadChildren: '../avatar/avatar.module#AvatarPageModule' },
-    { path: 'contact', loadChildren: '../list/list.module#ListPageModule' },
-    { path: 'settings', loadChildren: '../infinite-scroll/infinite-scroll.module#InfiniteScrollPageModule' }
+    { path: 'account', loadChildren: () => import('src/app/pages/avatar/avatar.module').then( m => m.AvatarPageModule) },
+    { path: 'contact', loadChildren: () => import('src/app/pages/list/list.module').then( m => m.ListPageModule) },
+    {
+      path: 'settings',
+      loadChildren: () => import('src/app/pages/infinite-scroll/infinite-scroll.module').then( m => m.InfiniteScrollPageModule)
+    }
   ]}
 ];
 
